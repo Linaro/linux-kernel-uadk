963095b0a86c7c688a762c6cd19d5ceda87edae1
Jean-Philippe Brucker <jean-philippe@linaro.org>
2020-04-14 17:53:27 +0200

    iommu/arm-smmu-v3: Add support for PRI
    For PCI devices that support it, enable the PRI capability and handle
    PRI Page Requests with the generic fault handler. It is enabled on
    demand by iommu_sva_device_init().
    
    Signed-off-by: Jean-Philippe Brucker <jean-philippe@linaro.org>

--- drivers/iommu/arm/arm-smmu-v3/arm-smmu-v3-sva.c
+++ drivers/iommu/arm/arm-smmu-v3/arm-smmu-v3-sva.c
@@ -368,6 +368,19 @@ arm_smmu_sva_bind(struct device *dev, struct mm_struct *mm, void *drvdata)
 void arm_smmu_sva_unbind(struct iommu_sva *handle)
 {
 	struct arm_smmu_bond *bond = sva_to_bond(handle);
+	struct arm_smmu_master *master = dev_iommu_priv_get(handle->dev);
+
+	/*
+	 * For stall, the event queue does not need to be flushed since the
+	 * device driver ensured all transaction are complete. For PRI however,
+	 * although the device driver has stopped all DMA for this PASID, it may
+	 * have left Page Requests in flight (if using the Stop Marker Message
+	 * to stop PASID). Complete them.
+	 */
+	if (master->pri_supported) {
+		arm_smmu_flush_priq(master->smmu);
+		iopf_queue_flush_dev(handle->dev);
+	}
 
 	mutex_lock(&sva_lock);
 	if (refcount_dec_and_test(&bond->refs)) {
@@ -437,7 +450,7 @@ bool arm_smmu_master_iopf_supported(struct arm_smmu_master *master)
 	if (master->num_streams != 1)
 		return false;
 
-	return master->stall_enabled;
+	return master->stall_enabled || master->pri_supported;
 }
 
 bool arm_smmu_master_sva_supported(struct arm_smmu_master *master)
@@ -474,16 +487,31 @@ static int arm_smmu_master_sva_enable_iopf(struct arm_smmu_master *master)
 	if (!master->iopf_enabled)
 		return -EINVAL;
 
-	ret = iopf_queue_add_device(master->smmu->evtq.iopf, dev);
-	if (ret)
-		return ret;
+	if (master->stall_enabled) {
+		ret = iopf_queue_add_device(master->smmu->evtq.iopf, dev);
+		if (ret)
+			return ret;
+	} else if (master->pri_supported) {
+		ret = iopf_queue_add_device(master->smmu->priq.iopf, dev);
+		if (ret)
+			return ret;
+
+		ret = arm_smmu_enable_pri(master);
+		if (ret)
+			goto err_remove_device;
+	}
 
 	ret = iommu_register_device_fault_handler(dev, iommu_queue_iopf, dev);
-	if (ret) {
-		iopf_queue_remove_device(master->smmu->evtq.iopf, dev);
-		return ret;
-	}
+	if (ret)
+		goto err_disable_pri;
 	return 0;
+
+err_disable_pri:
+	arm_smmu_disable_pri(master);
+err_remove_device:
+	iopf_queue_remove_device(master->smmu->priq.iopf, dev);
+	iopf_queue_remove_device(master->smmu->evtq.iopf, dev);
+	return ret;
 }
 
 static void arm_smmu_master_sva_disable_iopf(struct arm_smmu_master *master)
@@ -494,6 +522,8 @@ static void arm_smmu_master_sva_disable_iopf(struct arm_smmu_master *master)
 		return;
 
 	iommu_unregister_device_fault_handler(dev);
+	arm_smmu_disable_pri(master);
+	iopf_queue_remove_device(master->smmu->priq.iopf, dev);
 	iopf_queue_remove_device(master->smmu->evtq.iopf, dev);
 }
 

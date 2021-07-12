ddc133acf3fd8502a60d40fca393ce76a77928c6
Zhangfei Gao <zhangfei.gao@linaro.org>
2021-07-12 05:47:35 +0000

    qm: support use noiommu mode
    Signed-off-by: Kai Ye <yekai13@huawei.com>
    Signed-off-by: Zhangfei Gao <zhangfei.gao@linaro.org>

--- drivers/crypto/hisilicon/qm.h
+++ drivers/crypto/hisilicon/qm.h
@@ -7,6 +7,7 @@
 #include <linux/iopoll.h>
 #include <linux/module.h>
 #include <linux/pci.h>
+#include <linux/uacce.h>
 
 #define QM_QNUM_V1			4096
 #define QM_QNUM_V2			1024
@@ -263,8 +264,10 @@ struct hisi_qm {
 	struct work_struct rst_work;
 	struct work_struct cmd_process;
 
+	bool use_uacce;        /* register to uacce */
 	const char *algs;
 	bool use_sva;
+	bool use_iommu;
 	bool is_frozen;
 
 	/* doorbell isolation enable */
@@ -370,7 +373,7 @@ static inline int mode_set(const char *val, const struct kernel_param *kp)
 		return -EINVAL;
 
 	ret = kstrtou32(val, 10, &n);
-	if (ret != 0 || (n != UACCE_MODE_SVA &&
+	if (ret != 0 || (n != UACCE_MODE_NOIOMMU && n != UACCE_MODE_SVA &&
 			 n != UACCE_MODE_NOUACCE))
 		return -EINVAL;
 
@@ -388,6 +391,7 @@ static inline void hisi_qm_init_list(struct hisi_qm_list *qm_list)
 	mutex_init(&qm_list->lock);
 }
 
+int qm_register_uacce(struct hisi_qm *qm);
 int hisi_qm_init(struct hisi_qm *qm);
 void hisi_qm_uninit(struct hisi_qm *qm);
 int hisi_qm_start(struct hisi_qm *qm);

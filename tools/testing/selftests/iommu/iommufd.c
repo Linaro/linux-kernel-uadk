fab524b7054fc38d71b6d7a46f90e1f97538d2eb
Nicolin Chen <nicolinc@nvidia.com>
2024-04-04 03:01:31 +0000

    iommufd/selftest: Add IOMMU_VDEVICE_ALLOC test coverage
    Add a vdevice_alloc op to the viommu mock_viommu_ops for the coverage of
    IOMMU_VIOMMU_TYPE_SELFTEST allocations. Then, add a vdevice_alloc TEST_F
    to cover the IOMMU_VDEVICE_ALLOC ioctl.
    
    Signed-off-by: Nicolin Chen <nicolinc@nvidia.com>

--- tools/testing/selftests/iommu/iommufd.c
+++ tools/testing/selftests/iommu/iommufd.c
@@ -129,6 +129,7 @@ TEST_F(iommufd, cmd_length)
 	TEST_LENGTH(iommu_option, IOMMU_OPTION, val64);
 	TEST_LENGTH(iommu_vfio_ioas, IOMMU_VFIO_IOAS, __reserved);
 	TEST_LENGTH(iommu_viommu_alloc, IOMMU_VIOMMU_ALLOC, out_viommu_id);
+	TEST_LENGTH(iommu_vdevice_alloc, IOMMU_VDEVICE_ALLOC, __reserved2);
 #undef TEST_LENGTH
 }
 
@@ -2471,6 +2472,25 @@ FIXTURE_VARIANT_ADD(iommufd_viommu, mock_viommu)
 
 TEST_F(iommufd_viommu, viommu_auto_destroy)
 {
+}
+
+TEST_F(iommufd_viommu, vdevice_alloc)
+{
+	uint32_t viommu_id = self->viommu_id;
+	uint32_t dev_id = self->device_id;
+	uint32_t vdev_id = 0;
+
+	if (dev_id) {
+		/* Set vdev_id to 0x99, unset it, and set to 0x88 */
+		test_cmd_vdevice_alloc(viommu_id, dev_id, 0x99, &vdev_id);
+		test_err_vdevice_alloc(EEXIST, viommu_id, dev_id, 0x99,
+				       &vdev_id);
+		test_ioctl_destroy(vdev_id);
+		test_cmd_vdevice_alloc(viommu_id, dev_id, 0x88, &vdev_id);
+		test_ioctl_destroy(vdev_id);
+	} else {
+		test_err_vdevice_alloc(ENOENT, viommu_id, dev_id, 0x99, NULL);
+	}
 }
 
 TEST_HARNESS_MAIN

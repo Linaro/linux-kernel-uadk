514ea910703ba67d0fbaa36f52bcaa6ee123eb85
Johannes Weiner <hannes@cmpxchg.org>
2024-12-18 17:34:58 +0800

    mm: page_alloc: batch vmstat updates in expand()
    mainline inclusion
    from mainline-v6.10-rc1
    commit 883dd161e9a83e188487debc562b1928917a4b39
    category: performance
    bugzilla: https://gitee.com/openeuler/kernel/issues/ID3Q7D
    
    Reference: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=883dd161e9a83e188487debc562b1928917a4b39
    
    --------------------------------
    
    expand() currently updates vmstat for every subpage.  This is unnecessary,
    since they're all of the same zone and migratetype.
    
    Count added pages locally, then do a single vmstat update.
    
    Link: https://lkml.kernel.org/r/20240327190111.GC7597@cmpxchg.org
    Signed-off-by: Johannes Weiner <hannes@cmpxchg.org>
    Suggested-by: Vlastimil Babka <vbabka@suse.cz>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Liu Shixin <liushixin2@huawei.com>
    
    Conflicts:
    	mm/page_alloc.c
    [Context conflicts.]
    Signed-off-by: Jinjiang Tu <tujinjiang@huawei.com>

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -1339,13 +1339,11 @@ struct page *__pageblock_pfn_to_page(unsigned long start_pfn,
  *
  * -- nyc
  */
-static inline void add_to_free_list(struct page *page, struct zone *zone,
-				    unsigned int order, int migratetype,
-				    bool tail);
 static inline void expand(struct zone *zone, struct page *page,
 	int low, int high, int migratetype)
 {
 	unsigned long size = 1 << high;
+	unsigned long nr_added = 0;
 
 	while (high > low) {
 		high--;
@@ -1361,9 +1359,11 @@ static inline void expand(struct zone *zone, struct page *page,
 		if (set_page_guard(zone, &page[size], high))
 			continue;
 
-		add_to_free_list(&page[size], zone, high, migratetype, false);
+		__add_to_free_list(&page[size], zone, high, migratetype, false);
 		set_buddy_order(&page[size], high);
+		nr_added += size;
 	}
+	account_freepages(zone, nr_added, migratetype);
 }
 
 static void check_new_page_bad(struct page *page)
